92dbd5aff85d4eb1533cb382d76f167d33f4be67
marc <marc.ni@outlook.de>
2025-01-22 16:14:12 +0100

    Rename database + bash File for starting the node js server

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const next = require('next');
 const path = require('path');
 
-const port = 5000;
+const port = 5001;
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev, dir: path.join(__dirname, '../frontend') });
---
+++ backend/serverStart.sh
@@ -0,0 +1,5 @@
+#File to start the npm Server 
+# Execute with source serverStart.sh
+npm install
+node database.js
+node index.js 
