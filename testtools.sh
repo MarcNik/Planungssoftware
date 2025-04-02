948ddc0f00233caa0d3bf446d67c69b8218ffad7
Marc Nikolic <marc.ni@outlook.de>
2025-04-02 22:27:39 +0200

    Unit testing not working right now

--- testtools.sh
+++ testtools.sh
@@ -1,3 +1,4 @@
 npm install cypress --save-dev
 npm install --save-dev jest
+npm install @testing-library/react @testing-library/jest-dom @testing-library/user-event jest
 sudo npm install -g artillery
